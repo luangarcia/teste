5f5212d2d653db0beaacf6c65f03cc47f38c766d
Luan Garcia <luan.garcia@gmail.com>
2024-07-11 16:10:22 -0300

    init

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const puppeteer = require('puppeteer-extra');
+const puppeteer = require('puppeteer-firefox');
 const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
@@ -9,17 +9,15 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const app = express();
 const port = 3003;
 
-puppeteer.use(StealthPlugin());
-
 const apiKey = '712a38ac18fbe493d415b6d2ffd898d1';
 const siteDetails = {
   sitekey: '4a65992d-58fc-4812-8b87-789f7e7c4c4b',
   pageurl: 'https://solucoes.receita.fazenda.gov.br/Servicos/certidaointernet/PJ/Consultar'
 };
 
-const chromeOptions = {
+const firefoxOptions = {
   slowMo: 100, // Mantém a lentidão para simular interações humanas
-  headless: 'new', // Modo não-headless para simular um navegador real
+  headless: false, // Modo não-headless para simular um navegador real
   args: [
     '--no-sandbox',
     '--disable-setuid-sandbox',
@@ -28,8 +26,7 @@ const chromeOptions = {
     '--window-position=0,0',
     '--ignore-certifcate-errors',
     '--ignore-certifcate-errors-spki-list',
-    '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    '--disable-blink-features=AutomationControlled',
+    '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
     '--disable-extensions',
     '--start-maximized'
   ],
@@ -69,9 +66,7 @@ const timeout = millis => new Promise(resolve => setTimeout(resolve, millis));
 
 const run = async (url, cnpj, dataInicio, dataFim) => {
 
-  // const browser = await puppeteer.connect({ browserWSEndpoint: 'ws://localhost:3000' });
-
-  const browser = await puppeteer.launch(chromeOptions);
+  const browser = await puppeteer.launch(firefoxOptions);
   const page = await browser.newPage();
   await page.evaluateOnNewDocument(() => {
     Object.defineProperty(navigator, 'webdriver', {
@@ -82,43 +77,31 @@ const run = async (url, cnpj, dataInicio, dataFim) => {
     await page.goto(url, { waitUntil: 'networkidle2' });
     console.log('Página carregada, preenchendo formulário.');
 
-    
-    // await page.keyboard.type('#Ni', cnpj)
-       // Set the value of the #Ni input field directly
-      //  await page.evaluate((cnpj) => {
-      //   document.querySelector('#Ni').value = cnpj;
-      // }, cnpj);
-
-      // Espera explícita para garantir que os elementos estejam presentes
-      await page.waitForSelector('#Ni', { visible: true, timeout: 30000 });
-      await page.waitForSelector('#PeriodoInicio', { visible: true, timeout: 30000 });
-      await page.waitForSelector('#PeriodoFim', { visible: true, timeout: 30000 });
+    await page.waitForSelector('#Ni', { visible: true, timeout: 30000 });
+    await page.waitForSelector('#PeriodoInicio', { visible: true, timeout: 30000 });
+    await page.waitForSelector('#PeriodoFim', { visible: true, timeout: 30000 });
   
-      await page.evaluate((cnpj) => {
-        const input = document.querySelector('#Ni');
-        input.value = cnpj;
-        input.focus();
-        input.blur(); 
-      }, cnpj);
+    await page.evaluate((cnpj) => {
+      const input = document.querySelector('#Ni');
+      input.value = cnpj;
+      input.focus();
+      input.blur(); 
+    }, cnpj);
   
-      await page.evaluate((dataInicio) => {
-        const input = document.querySelector('#PeriodoInicio');
-        input.value = dataInicio;
-        input.focus();
-        input.blur(); 
-      }, dataInicio);
+    await page.evaluate((dataInicio) => {
+      const input = document.querySelector('#PeriodoInicio');
+      input.value = dataInicio;
+      input.focus();
+      input.blur(); 
+    }, dataInicio);
   
-      await page.evaluate((dataFim) => {
-        const input = document.querySelector('#PeriodoFim');
-        input.value = dataFim;
-        input.focus();
-        input.blur(); 
-      }, dataFim);
+    await page.evaluate((dataFim) => {
+      const input = document.querySelector('#PeriodoFim');
+      input.value = dataFim;
+      input.focus();
+      input.blur(); 
+    }, dataFim);
   
-
-//  await page.type('#Ni', cnpj);
-//     await page.type('#PeriodoInicio', dataInicio);
-//     await page.type('#PeriodoFim', dataFim);
     const requestId = await initiateCaptchaRequest();
     const captchaSolution = await pollForRequestResults(apiKey, requestId);
     console.log('captchaSolution', captchaSolution);
@@ -128,9 +111,7 @@ const run = async (url, cnpj, dataInicio, dataFim) => {
       document.querySelector('[name="h-captcha-response"]').value = captchaSolution;
     }, captchaSolution);
     await page.click('#validar');
-    // await page.waitForNavigation({ waitUntil: 'networkidle2' });
-
-    // Set up download behavior
+    
     await page._client().send('Page.setDownloadBehavior', {
       behavior: 'allow',
       downloadPath: '/tmp',
@@ -139,9 +120,8 @@ const run = async (url, cnpj, dataInicio, dataFim) => {
 
     await page.screenshot({ path: 'screenshot1.png' });
 
-    await page.waitForNavigation({ waitUntil: 'networkidle0' })
+    await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
-    // Adicionar captura de tela para depuração
     await page.screenshot({ path: 'screenshot2.png' });
 
     await page.waitForSelector('.fileDownloadAlerta', { timeout: 120000 }); 
@@ -150,7 +130,6 @@ const run = async (url, cnpj, dataInicio, dataFim) => {
 
     await page.waitForSelector('.ui-dialog-buttonset', { timeout: 120000 }); 
 
-    // Find the downloaded PDF file
     const files = fs.readdirSync('/tmp');
     const pdfFile = files.find(file => file.endsWith('.pdf'));
     if (!pdfFile) {
@@ -160,16 +139,14 @@ const run = async (url, cnpj, dataInicio, dataFim) => {
     const pdfContent = fs.readFileSync(pdfPath);
     const pdfBase64 = pdfContent.toString('base64');
     
-    // Clean up the downloaded file
     fs.unlinkSync(pdfPath);
 
-    // Adicionar captura de tela para depuração
     await page.screenshot({ path: 'screenshot3.png' });
-     await browser.close();
+    await browser.close();
     return pdfBase64;
   } catch (error) {
     console.error('Erro durante a execução do Puppeteer:', error);
-      await browser.close();
+    await browser.close();
     throw error;
   }
 };
@@ -198,3 +175,4 @@ app.get('/', async function (req, res) {
 app.listen(port, '0.0.0.0', function () {
   console.log(`App ouvindo na porta ${port}!`);
 });
+
